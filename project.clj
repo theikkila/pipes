41a8cbd7328514323c5f5b0066aae191e87d642a
Teemu Heikkilä <teemu@emblica.fi>
2016-06-22 01:40:42 +0300

    Add http-kit server, environ, logging and zero-route

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "https://github.com/theikkila/pipes"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.1.18"]
+                 [com.taoensso/timbre "4.4.0"]
+                 [environ "1.0.3"]
+                 [compliment "0.2.7"]
+                 [compojure "1.5.1"]]
+  :plugins [[lein-environ "1.0.3"]]
+  :repl-options {:init (require 'clojure.tools.namespace.repl)}
   :main ^:skip-aot pipes.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:env {:port "8080"}}})
--- src/pipes/core.clj
+++ src/pipes/core.clj
@@ -1,7 +1,19 @@
 (ns pipes.core
+  (:require [taoensso.timbre :refer [log  trace  debug  info  warn  error  fatal  report]]
+            [environ.core :refer [env]])
+  (:use org.httpkit.server)
   (:gen-class))
 
+
+(def PORT (Integer/parseInt (env :port)))
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Pipes-server main function"
   [& args]
-  (println "Hello, World!"))
+  (log "Starting server at " PORT)
+  (run-server app {:port PORT}))
